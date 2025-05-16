a73f1735c4532f12bcf1f82c6b9ebcacc784aa26
BobbyLin <linzhangsheng23@gmail.com>
2025-05-16 23:26:10 +0800

    chore: add ui and eslint config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,21 @@
+import tailwindcss from '@tailwindcss/vite'
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  css: ['~/assets/css/tailwind.css'],
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+  modules: ['shadcn-nuxt'],
+  devServer: {
+    port: 3011,
+  },
+  shadcn: {
+    prefix: '',
+    componentDir: '~/components/ui',
+  },
 })
