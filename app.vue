dad1cf037b6f67f58e7114c5c7ffb37dca797bda
BobbyLin <linzhangsheng23@gmail.com>
2025-05-17 09:18:34 +0800

    feat: add site head and meta

--- app.vue
+++ app.vue
@@ -1,3 +1,28 @@
+<script setup lang="ts">
+useHead({
+  titleTemplate: (titleChunk) => {
+    return titleChunk ? `${titleChunk} %separator %siteName` : '%siteName'
+  },
+  templateParams: {
+    siteName: 'Taskify',
+    separator: '-',
+  },
+  meta: [
+    {
+      name: 'description',
+      content: 'Taskify is a task management app that helps you manage your tasks and projects.',
+    },
+  ],
+  link: [
+    {
+      rel: 'icon',
+      type: 'image/x-icon',
+      href: '/logo.svg',
+    },
+  ],
+})
+</script>
+
 <template>
   <NuxtLayout>
     <NuxtPage />
